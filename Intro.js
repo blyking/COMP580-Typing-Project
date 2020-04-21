cf065b15a174d9024dd2533c65eacbb05a1eb894
jvanglish <jordan.vanglish@gmail.com>
2020-04-21 15:59:25 -0400

    Added words

--- Intro.js
+++ Intro.js
@@ -57,7 +57,7 @@ function testCorrect(answer, key) {
 }
 
 function pickWordOrPhrase() { //Text generator for mode 2
-    var words = ["Hello", "World", "Puppy", "Alphabet", "Chemistry", "Beach", "Kitten", "Teacher", "School", "Fox", "Blue", "Color", "Sky", "Lake", "Skateboard", "Whisper", "President", "Government", "Whom", "No", "Thanks", "Cold", "Weather", "Please"];
+    var words = ["Hello", "World", "Puppy", "Alphabet", "Chemistry", "Beach", "Kitten", "Teacher", "School", "Fox", "Blue", "Color", "Sky", "Lake", "Skateboard", "Whisper", "President", "Government", "Whom", "No", "Thanks", "Cold", "Weather", "Please", "Math", "Example", "Measure", "Friend", "Country", "Picture", "Women", "Men", "Elephant", "Water", "People", "Dragon", "Animal", "Lunch", "Basket", "Owl", "Nowhere", "Ladder", "Orange", "Blue", "Green", "Blue", "Yellow", "Birthday", "Present", "Letter", "Pencil", "Paper"];
     var phrases = ["Thank you", "Good luck", "What is your name", "Once in a blue moon", "Never say never", "Where is the library"];
     var wordorphrase = (Math.random() * 2);
     if (wordorphrase <= 1) {
